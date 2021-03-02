4e719fdc63a7c4ae55d0431db1868eb3b15c1bba
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-03-02 14:27:44 +0200

    Add: template for the scheduler module.

---
+++ template/sch.h
@@ -0,0 +1,29 @@
+/**
+ * @file sch.h
+ * @author Mohamed Hassanin 
+ * @brief A header file for cooperative scheduler module.
+ * @version 0.1
+ * @date 2021-03-02
+ */
+
+#ifndef SCH_H
+#define SCH_H
+/**********************************************************************
+* Includes
+**********************************************************************/
+#include <inttypes.h>
+/**********************************************************************
+* Module Preprocessor Constants
+**********************************************************************/
+/**********************************************************************
+* Function Prototypes
+**********************************************************************/
+void Sch_Init(void);
+void Sch_Deinit(void);
+uint8_t Sch_AddTask(void (*Task) (void), const uint32_t Delay, const uint32_t Interval);
+void Sch_DeleteTask(const uint8_t TaskId);
+void Sch_Start(void);
+void Sch_Update(void);
+
+#endif /* end SCH_H */
+/************************* END OF FILE ********************************/
---
+++ template/sch_cfg.h
@@ -0,0 +1,21 @@
+/**
+ * @file sch_cfg.h
+ * @author Mohamed Hassanin 
+ * @brief A configuration header file for cooperative scheduler module.
+ * @version 0.1
+ * @date 2021-03-02
+ */
+
+#ifndef SCH_CFG_H
+#define SCH_CFG_H
+/**********************************************************************
+* Preprocessor Constants
+**********************************************************************/
+/*< the tick value in milliseconds. */
+#define TICK 10
+
+/*< The maximum number of tasks in the project */
+#define SCH_MAX_TASKS (2)
+
+#endif /* end SCH_CFG_H */
+/************************* END OF FILE ********************************/
